bfbb29fbb576fc2ec2080f5fd110da3cb53216be
31O <rs310000@gmail.com>
2018-03-25 22:23:18 +0300

    Added basic interface for employee login

--- src/sample/ControllerLoginAsEmployee.java
+++ src/sample/ControllerLoginAsEmployee.java
@@ -1,4 +1,12 @@
 package sample;
 
+import java.awt.*;
+
 public class ControllerLoginAsEmployee {
+    TextField clientNameField = new TextField();
+
+    public TextField getClientNameField() {
+        clientNameField.setText("-fx-text-inner-color: red;");
+       return clientNameField;
+    }
 }
