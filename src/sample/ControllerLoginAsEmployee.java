7f45e48be0b916dc481f389b57df976ec64cfa4f
31O <rs310000@gmail.com>
2018-04-03 00:04:21 +0300

    added messages

--- src/sample/ControllerLoginAsEmployee.java
+++ src/sample/ControllerLoginAsEmployee.java
@@ -7,7 +7,10 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -16,30 +19,48 @@ public class ControllerLoginAsEmployee implements Initializable {
     public TextField txtTimeSpent;
     public Button btnWriteData;
     public DatePicker dtpDate;
-    public Label lblIfActionSuccessful;
+    public Label lblMessages;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
 
-    public void WriteData(ActionEvent event) throws FileNotFoundException {
-        File employeeData = new File("employeeData.txt");
-        try {
-            if (!employeeData.exists()) {
-                employeeData.createNewFile();
-            }
-            PrintWriter fileWriter = new PrintWriter(new FileWriter(employeeData, true));
-            fileWriter.append(txtClientName.getText() + " " + txtTimeSpent.getText() + " " + dtpDate.getValue());
-            fileWriter.println();
-            fileWriter.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        txtClientName.clear();
-        txtTimeSpent.clear();
-        lblIfActionSuccessful.setText("Data successfully stored");
+    public void WriteData(ActionEvent event) throws IOException {
 
+        /*
+        if(!txtClientName.getText().isEmpty() || !txtTimeSpent.getText().isEmpty() || dtpDate.getValue() == null) {
+            Parent root = FXMLLoader.load(getClass().getResource("successfullyStoredPopup.fxml"));
+            Stage primaryStage = new Stage();
+            primaryStage.setTitle("Data stored");
+            primaryStage.setScene(new Scene(root, 200, 100));
+            primaryStage.show();
+            primaryStage.setResizable(true);
+        }
+        */
+        if(txtClientName.getText().isEmpty() || txtTimeSpent.getText().isEmpty() || dtpDate.getValue() == null) {
+            lblMessages.setText("Please enter correct information!");
+
+        }
+        else {
+            File employeeData = new File("employeeData.txt");
+            try {
+                if (!employeeData.exists()) {
+                    employeeData.createNewFile();
+                }
+                PrintWriter fileWriter = new PrintWriter(new FileWriter(employeeData, true));
+                fileWriter.append(txtClientName.getText() + " " + txtTimeSpent.getText() + " " + dtpDate.getValue());
+                fileWriter.println();
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            lblMessages.setText("Data for " + txtClientName.getText() + " successfully stored!");
+            txtClientName.clear();
+            txtTimeSpent.clear();
+        }
     }
+
 }
 
