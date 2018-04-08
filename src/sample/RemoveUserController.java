d7db0a45e295d1c5d6c25f8a8db6dddf71122245
31O <rs310000@gmail.com>
2018-04-08 21:18:22 +0300

    adding pics

---
+++ src/sample/RemoveUserController.java
@@ -0,0 +1,75 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RemoveUserController implements Initializable {
+
+    public ComboBox cmbSelectCustomer;
+    public Button btnRemoveClient;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("Customers"));
+            String strLine;
+            int flag =0;
+            if (flag == 0) {
+                while ((strLine = br.readLine()) != null) {
+                    cmbSelectCustomer.getItems().addAll(strLine);
+                }
+                flag = 1;
+                removeEmptyline();
+            }
+            br.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+    }
+
+    public void btnRemove(ActionEvent event) {
+        String removeCustomer = (String) cmbSelectCustomer.getValue();
+
+        try {
+            Path path = Paths.get("Customers");
+            Stream<String> lines = Files.lines(path);
+            List<String> replaced = lines.map(line -> line.replaceAll(removeCustomer, "")).collect(Collectors.toList());
+            Files.write(path, replaced);
+            cmbSelectCustomer.getItems().remove(removeCustomer);
+            lines.close();
+            System.out.println("Find and Replace done!!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void removeEmptyline() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader("Customers"));
+        String line = "";
+        String out = "";
+        while ((line = reader.readLine()) != null) {
+            if (line.length() > 0) {
+                out += line + "\n";
+            }
+        }
+        reader.close();
+        BufferedWriter writer = new BufferedWriter(new FileWriter("Customers"));
+        writer.append(out);
+        writer.close();
+        System.out.println(out);
+    }
+}
+
